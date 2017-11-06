4c3279772e70b8e28e7c499de2ea868dd1e08fe2
wei <903889108@qq.com>
2017-11-06 11:01:44 +0800

    just a test

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/11/6
+ * Time: 10:54
+ */
+namespace App\Http\Controllers;
+
+class StudentController extends Controller
+{
+    public function info(){
+        return '什么鬼的啊';
+    }
+}
